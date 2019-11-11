36f7105a3059d288bf0deb67b5c0abbd4fb70f49
Alexandre Eher <alexandre@eher.com.br>
2019-11-11 09:08:27 +0100

    Extract logic to post a json
    Signed-off-by: Alexandre Eher <alexandre@eher.com.br>

--- main.go
+++ main.go
@@ -8,25 +8,28 @@ import (
     "os"
 )
 
-func main() {
-    url := os.Getenv("WEBHOOK_URL")
-    fmt.Println("URL:>", url)
-
-    var jsonStr = []byte(`{"text":"` + os.Getenv("INVITE_MESSAGE") + `"}`)
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+func postJson(url string, json []byte) (error) {
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
     req.Header.Set("Content-Type", "application/json")
     client := &http.Client{}
     resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
     fmt.Println("response Status:", resp.Status)
     fmt.Println("response Headers:", resp.Header)
     body, _ := ioutil.ReadAll(resp.Body)
     fmt.Println("response Body:", string(body))
     if err != nil {
         fmt.Println(err)
+    }
+    defer resp.Body.Close()
+    return err
+}
+
+func main() {
+    var url = os.Getenv("WEBHOOK_URL")
+    var json = []byte(`{"text":"` + os.Getenv("INVITE_MESSAGE") + `"}`)
+    err := postJson(url, json)
+    if err != nil {
+        panic(err)
     }
     fmt.Printf("Message successfully sent")
 }
